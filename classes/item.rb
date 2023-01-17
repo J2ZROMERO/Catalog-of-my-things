f5083cae79df2b7bb396c2fde18635d2363d86cf
Jhosep <jose-zromero@hotmail.com>
2023-01-17 10:53:10 -0600

    Fix linters

--- classes/item.rb
+++ classes/item.rb
@@ -2,53 +2,48 @@ require 'securerandom'
 require 'date'
 
 class Item
- attr_accessor :publish_date
- attr_reader :genre, :author, :label, :source , :archived
+  attr_accessor :publish_date
+  attr_reader :genre, :author, :label, :source, :archived
 
-def initialize(publish_date)
-  @id = SecureRandom.uuid
-  @publish_date = publish_date
-  @archived = false
-end
+  def initialize(publish_date)
+    @id = SecureRandom.uuid
+    @publish_date = publish_date
+    @archived = false
+  end
 
-def add_genre(genre)
+  def add_genre(genre)
     @genre = genre
     genre.add_item(genre)
- end
-    
- def add_source(source)
+  end
+
+  def add_source(source)
     @source = source
     source.add_item(source)
- end
- 
- def add_genre(label)
+  end
+
+  def add_label(label)
     @label = label
     label.add_item(label)
- end
+  end
 
- def add_genre(author)
+  def add_author(author)
     @author = author
     author.add_item(author)
- end
+  end
 
- def move_to_archive()
-    if can_be_archived? 
-      @archived = true
-    else
-      @archived = false
-    end
- end
+  def move_to_archive()
+    @archived = can_be_archived?
+  end
 
- private
- def can_be_archived?
-    if (Date.today.year - Date.parse(@publish_date).year).to_i > 10 
-      return true
-    else
-      return false
-    end
+  private
+
+  def can_be_archived?
+    return true if (Date.today.year - Date.parse(@publish_date).year).to_i > 10
+
+    false
   end
 end
 
-item = Item.new("Jan 03 2020")
+item = Item.new('Jan 03 2020')
 puts item.publish_date
 puts item.move_to_archive
