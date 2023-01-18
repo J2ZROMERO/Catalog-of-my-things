602aebc669418136eb8667d2360eacdcfe239f90
Oklukeok <lucasabermudez@gmail.com>
2023-01-18 09:21:18 -0600

    Test units for genre and music_album

---
+++ spec/musicalbum_spec.rb
@@ -0,0 +1,18 @@
+require './classes/music_album.rb'
+require './classes/item.rb'
+describe MusicAlbum do
+    context 'check for correct values passed through MusicAlbum class' do
+    new_album = MusicAlbum.new('01 jan 2023', onspotify: false)
+    it 'check for correct values passed through MusicAlbum class' do
+      expect(new_album.publish_date).to eql('01 jan 2023')
+    end
+    it 'check for correct values passed through MusicAlbum class' do
+        expect(new_album.onspotify).to eql(false)
+      end
+      it 'Checking if the album is false' do
+        expect(new_album.move_to_archive).to eql(false)
+      end
+     
+    end
+  end
+  
