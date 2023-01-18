0559e9d7ccfd4f0ffaf548a757625b306a9e2fb2
gandradep <eventsonido@gmail.com>
2023-01-18 11:17:44 -0600

    Set structure for user interface

---
+++ app.rb
@@ -0,0 +1,57 @@
+class App
+  def initialize
+    # should read from file
+    @list_books = []
+    @list_albums = []
+    @list_games = []
+    @list_movies = []
+  end
+
+  def show_books
+    # code comes from module
+  end
+
+  def show_albums
+    # code comes from module
+  end
+
+  def show_movies
+    # code comes from module
+  end
+
+  def show_games
+    # code comes from module
+  end
+
+  def show_genres
+    # code comes from module
+  end
+
+  def show_labels
+    # code comes from module
+  end
+
+  def show_authors
+    # code comes from module
+  end
+
+  def show_sources
+    # code comes from module
+  end
+
+  def add_book
+    # code comes from module
+  end
+
+  def add_album
+    # code comes from module
+  end
+
+  def add_movie
+    # code comes from module
+  end
+
+  def add_game
+    # code comes from module
+  end
+end
--- main.rb
+++ main.rb
@@ -1,1 +1,40 @@
+require './select_method'
+# this class runs the user interface
+class Main
+  def initialize
+    @select_method = SelectMethod.new
+    puts 'My items collection!'
+    @select_method.load_json
+    @menu = <<~TEXT
+      Please choose an option by entering a number:
+       1 - List all books
+       2 - List all music albums
+       3 - List all movies
+       4 - List of games
+       5 - List all genres (e.g 'Comedy', 'Thriller')
+       6 - List all labels (e.g. 'Gift', 'New')
+       7 - List all authors (e.g. 'Stephen King')
+       8 - List all sources (e.g. 'From a friend', 'Online shop')
+       9 - Add a book
+      10 - Add a music album
+      11 - Add a movie
+      12 - Add a game
+       0 - Exit
+    TEXT
+  end
 
+  def main
+    puts @menu
+    user_input = gets.chomp.to_i
+    if user_input.zero?
+      @select_method.save_json
+      puts 'Thank you for using this app!'
+    else
+      @select_method.assign_method(user_input)
+      main
+    end
+  end
+end
+
+start = Main.new
+start.main
