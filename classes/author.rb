9deda3e2422b46eceb7cb77d6fc655a9b8aca539
amare1990 <amaremek@gmail.com>
2023-01-18 15:49:28 +0300

    Fix bugs in author.rb file

--- classes/author.rb
+++ classes/author.rb
@@ -1,9 +1,10 @@
 require './classes/item'
+
 class Author
   attr_accessor :first_name, :last_name, :items
 
-  def initialize(first_name, last_name)
-    @id = Random.rand(1..1000)
+  def initialize(first_name, last_name, id: Random.rand(1..1000).to_i)
+    @id = id
     @first_name = first_name
     @last_name = last_name
     @items = []
@@ -13,10 +14,5 @@ class Author
     @items.push(item) unless @items.include?(item)
     item.add_author(self)
   end
-end
 
-auth = Author.new('Amare', 'kassa')
-item = Item.new('2022-01-14')
-puts "#{item} item"
-auth.add_item(item)
-puts auth.items
+end
