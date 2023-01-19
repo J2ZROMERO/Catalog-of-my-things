f1975a442abf75a6dc7cb81b5bdc9d1036766c86
Jhosep <jose-zromero@hotmail.com>
2023-01-19 14:35:13 -0600

    Refactor schema.sql file

--- database/schema.sql
+++ database/schema.sql
@@ -1,20 +1,24 @@
 CREATE TABLE book (
 	id int  generated always as identity NOT NULL,
-	publisher varchar NULL,
-	cover_state varchar NULL,
-	publish_date varchar NULL,
-	archived boolean NULL,
-	fk_author int NULL,
-	fk_label int NULL,
+	publisher varchar,
+	cover_state int,
+	publish_date date,
+	archived boolean,
+	author_id int,
+	label_id int,
+	source_id int,
+	genre_id int
 	CONSTRAINT book_pk PRIMARY KEY (id),
-	CONSTRAINT book_fk FOREIGN KEY (fk_label) REFERENCES public."label"(id) ON DELETE SET NULL ON UPDATE CASCADE,
-	CONSTRAINT book_fk_1 FOREIGN KEY (fk_author) REFERENCES public.author(id) ON DELETE SET NULL ON UPDATE CASCADE
+	CONSTRAINT book_fk FOREIGN KEY (label_id) REFERENCES label(id) ON DELETE SET NULL ON UPDATE CASCADE,
+	CONSTRAINT book_fk_1 FOREIGN KEY (author_id) REFERENCES author(id) ON DELETE SET NULL ON UPDATE CASCADE
+	CONSTRAINT book_fk_2 FOREIGN KEY (source_id) REFERENCES author(id) ON DELETE SET NULL ON UPDATE CASCADE
+	CONSTRAINT book_fk_3 FOREIGN KEY (genre_id) REFERENCES author(id) ON DELETE SET NULL ON UPDATE CASCADE
+
 );
 
 CREATE TABLE label (
 	id int  generated always as identity NOT NULL,
-	title varchar NULL,
-	color varchar NULL,
-	items varchar NULL,
+	title varchar,
+	color varchar,
 	CONSTRAINT label_pk PRIMARY KEY (id)
 );
