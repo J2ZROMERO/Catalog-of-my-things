0005c7dbcfbb831e461dabcc73c91a1332258040
Jhosep <jose-zromero@hotmail.com>
2023-01-19 14:29:35 -0600

    remove author table

--- database/schema.sql
+++ database/schema.sql
@@ -18,13 +18,3 @@ CREATE TABLE label (
 	items varchar NULL,
 	CONSTRAINT label_pk PRIMARY KEY (id)
 );
-
-
-CREATE TABLE author (
-	id int generated always as identity  NOT NULL,
-	first_name varchar NULL,
-	last_name varchar NULL,
-	items varchar NULL,
-	CONSTRAINT newtable_pk PRIMARY KEY (id)
-);
-
