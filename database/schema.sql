e9fd028d196f7bc3e15cb36158b744337c6735a3
amare1990 <amaremek@gmail.com>
2023-01-19 23:17:32 +0300

    Create author and game table

--- database/schema.sql
+++ database/schema.sql
@@ -1,19 +1,24 @@
-CREATE TABLE movie (
-	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-  published_date DATE NOT NULL,
-  archived BOOLEAN,
-  silent BOOLEAN,
-	source_id INTEGER,
-  author_id INTEGER,
-  genre_id INTEGER,
-  label_id INTEGER,
-  FOREIGN KEY(source_id) REFERENCES source(id),
-  FOREIGN KEY(author_id) REFERENCES author(id),
-  FOREIGN KEY(genre_id) REFERENCES genre(id),
-  FOREIGN KEY(label_id) REFERENCES label(id)
+CREATE TABLE AUTHOR (ID INT, first_name VARCHAR(100),
+last_name VARCHAR(100),
+PRIMARY KEY(ID)
 );
 
-CREATE TABLE source (
-  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-  source_name VARCHAR(80)
+CREATE GAME(author_ID INT NOT NULL, multiplayer BOOLEAN NOT NULL,
+last_played_at NOT NULL,
+
+
+publish_date DATE,
+archived BOOLEAN,
+
+author_ID INT,
+label_ID INT,
+genre_ID INT,
+source_ID INT,
+
+CONSTRAINT game_label FOREIGN KEY(label_ID ) REFERENCES label(id),
+CONSTRAINT game_auth FOREIGN KEY (author_ID) REFERENCES AUTHOR(ID),
+CONSTRAINT game_genre FOREIGN KEY (genre_ID) REFERENCES genre(ID),
+CONSTRAINT game_source FOREIGN KEY (source_ID) REFERENCES source(ID),
+
+PRIMARY KEY(ID)
 );
